cfa84220ec07d3d43879ea3c85df922d5334b278
Adam <asur0008@dyn-49-127-9-24.its.monash.edu.au>
2022-07-11 14:20:17 +1000

    11 July Working Version

--- main.py
+++ main.py
@@ -22,24 +22,17 @@ class Scanning_Probe(QMainWindow):
 
     def initUI(self):
         self.lcd = QLCDNumber(self)
-        btn1 = QPushButton("Run the Osilla Measurement", self)
-        btn1.setGeometry(100, 50, 200, 100)
-
-        btn2 = QPushButton("Run the Zaber Movement", self)
-        btn2.setGeometry(100, 150, 200, 100)
-
-        btn3 = QPushButton("Home position", self)
-        btn3.setGeometry(100, 250, 200, 100)
-
-        btn1.clicked.connect(self.osilla_run)
-        btn2.clicked.connect(self.zaber_run)
-        #btn3.clicked.connect(self.lcd_number)
-
         layout = QVBoxLayout()
         layout.addWidget(self.lcd)
-
-
         self.setLayout(layout)
+        btn1 = QPushButton("Run the Osilla Measurement", self)
+        btn1.setGeometry(100, 50, 200, 100)
+        btn2 = QPushButton("Run the Zaber Movement", self)
+        btn2.setGeometry(100, 150, 200, 100)
+        btn3 = QPushButton("Home position", self)
+        btn3.setGeometry(100, 250, 200, 100)
+        btn1.clicked.connect(self.osilla_run)
+        btn2.clicked.connect(self.zaber_run)
         btn3.clicked.connect(self.handleButton)
         #lcd.display(self.osilla_run())
         self.lcd.setGeometry(300, 300, 250, 150)
@@ -49,29 +42,24 @@ class Scanning_Probe(QMainWindow):
         self.show()
 
     def handleButton(self):
-        self.lcd.display(self.randomint())
-
+        self.lcd.display(self.osilla_run())
 
     def randomint(self):
         random = randint(2, 99)
         return random
 
-    def osilla_run(self, numb1=None):
+    def osilla_run(self):
         from measurement_movement import func1
         sender = self.sender()
-        #self.statusBar().showMessage(sender.text() + ' was pressed')
+        self.statusBar().showMessage(sender.text() + ' was pressed')
         position = float(input('Enter the desired position of the stage'))
         func1(position)
-        position = numb1
-        return numb1
+        return position
     def zaber_run(self):
         from measurement_movement import func2
         func2()
         sender = self.sender()
         self.statusBar().showMessage(sender.text() + ' was pressed')
-
-
-
 
 def main():
     app = QApplication(sys.argv)
