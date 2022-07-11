8392e076b1736880b1abde0835e974a4adfe7d75
Adam <asur0008@dyn-49-127-9-24.its.monash.edu.au>
2022-07-11 16:25:55 +1000

    11 July Working Version

--- file_saving.py
+++ file_saving.py
@@ -1,5 +1,8 @@
+import csv
+import math
 import simplejson as json
 import numpy as np
+import matplotlib.pyplot as plt
 
 from measurement_movement import func2 as osilla
 
@@ -15,6 +18,19 @@ with open ("measurement.csv", "a", newline='\n') as measurement:
 
 
 
+with open ("measurement.csv", newline='') as measurement:
+    reader = csv.reader(measurement)
+    arrX,arrY = [],[]
+    x_label = [0, 0.25, 0.5, 0.75, 1, 1.25]
+    for row in reader:
+        arrX.append(row[0].lstrip('['))
+        arrY.append(row[1].rstrip(']'))
+
+    plt.plot(arrX,arrY,'o')
+    #plt.xticks('0','0.5','1')
+    plt.ylabel('Current density [mA cm2]')
+    plt.xlabel('Voltage [v]')
+    plt.show()
 
 
 
